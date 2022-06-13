a33c6aeb3d65db576149900baaf93fc29709730b
Mikołaj Potera <mikolajpotera@gmail.com>
2022-06-13 16:49:43 +0200

    3. Day 1

---
+++ Source/WidgetPraktyki/MonsterBase.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+//#include "PaperSprite.h"
+#include "MonsterBase.generated.h"
+
+
+UCLASS()
+class WIDGETPRAKTYKI_API AMonsterBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMonsterBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//UPROPERTY(EditAnywhere)
+	//UPaperSprite* MySprite;
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
