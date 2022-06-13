c74860a4398d2d00c4f40e8ba2f27ae50c288acc
Mikołaj Potera <mikolajpotera@gmail.com>
2022-06-13 15:39:02 +0200

    1. Initial

---
+++ Source/WidgetPraktyki/WidgetPraktyki.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WidgetPraktyki.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WidgetPraktyki, "WidgetPraktyki" );
---
+++ Source/WidgetPraktyki/WidgetPraktykiGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WidgetPraktykiGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WIDGETPRAKTYKI_API AWidgetPraktykiGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
