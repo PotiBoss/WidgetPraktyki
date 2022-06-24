a43f58ae09daac20b59c4b0d3a9bf9ccf4ab6c9d
DESKTOP-P4D6QTM\Mikolaj <potigimboss@gmail.com>
2022-06-24 15:40:37 +0200

    30. Added randomized souls to inventory TODO: test, might crash

--- Source/WidgetPraktyki/InventoryWidget.cpp
+++ Source/WidgetPraktyki/InventoryWidget.cpp
@@ -3,3 +3,50 @@
 
 #include "InventoryWidget.h"
 
+#include "Components/Button.h"
+#include "Components/Image.h"
+
+
+void UInventoryWidget::InitWidget()
+{
+	InitButtons();
+}
+
+void UInventoryWidget::InitButtons()
+{
+	for(int i = 0; i < 19; i++)
+	{
+		InventorySoulsButtons.Add(ButtonSoul0);
+		InventorySoulsButtons.Add(ButtonSoul1);
+		InventorySoulsButtons.Add(ButtonSoul2);
+		InventorySoulsButtons.Add(ButtonSoul3);
+		InventorySoulsButtons.Add(ButtonSoul4);
+		InventorySoulsButtons.Add(ButtonSoul5);
+		InventorySoulsButtons.Add(ButtonSoul6);
+		InventorySoulsButtons.Add(ButtonSoul7);
+		InventorySoulsButtons.Add(ButtonSoul8);
+		InventorySoulsButtons.Add(ButtonSoul9);
+		InventorySoulsButtons.Add(ButtonSoul10);
+		InventorySoulsButtons.Add(ButtonSoul11);
+		InventorySoulsButtons.Add(ButtonSoul12);
+		InventorySoulsButtons.Add(ButtonSoul13);
+		InventorySoulsButtons.Add(ButtonSoul14);
+		InventorySoulsButtons.Add(ButtonSoul15);
+		InventorySoulsButtons.Add(ButtonSoul16);
+		InventorySoulsButtons.Add(ButtonSoul17);
+		InventorySoulsButtons.Add(ButtonSoul18);
+		InventorySoulsButtons.Add(ButtonSoul19);
+		
+	}
+	
+	for(int i = 0; i < InventorySoulsButtons.Num(); i++)
+	{
+		for (auto Button : InventorySoulsButtons)
+		{
+			int8 RandomSoul = FMath::RandRange(0,InventorySoulsSprites.Num() - 1);
+
+			UImage* Image = Cast<UImage>(Button->GetChildAt(0));
+			Image->SetBrushFromTexture(InventorySoulsSprites[RandomSoul]);
+		}
+	}
+}
--- Source/WidgetPraktyki/InventoryWidget.h
+++ Source/WidgetPraktyki/InventoryWidget.h
@@ -9,9 +9,71 @@
 /**
  * 
  */
+class UButton;
+class UImage;
+class UMyGameInstance;
+class UTexture2D;
 UCLASS()
 class WIDGETPRAKTYKI_API UInventoryWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+protected:
+
+	UFUNCTION(BlueprintCallable)
+	void InitWidget();
+
+	UFUNCTION()
+	void InitButtons();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<uint8> InventorySoulsID;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<UButton*> InventorySoulsButtons;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<UTexture2D*> InventorySoulsSprites;
 	
+	// WIDGET BUTTONS
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul0;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul1;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul2;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul3;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul4;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul5;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul6;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul7;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul8;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul9;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul10;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul11;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul12;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul13;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul14;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul15;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul16;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul17;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul18;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ButtonSoul19;
 };
